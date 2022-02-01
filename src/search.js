d2f7426d83b1b87c4345340a7d74a749a74078be
Ambz2 <ameenahmed1998@gmail.com>
2022-02-01 12:44:58 +0000

    changed mode from cors

--- src/search.js
+++ src/search.js
@@ -1,7 +1,7 @@
 export async function search(city = 'London', units = 'metric') {
     try {
         let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&APPID=a824f5a0abcab3b4759d19626fe7ba20`
-            , {mode:'cors'})
+            )
         let localeData = await response.json()
         return localeData
     } catch(err) {
